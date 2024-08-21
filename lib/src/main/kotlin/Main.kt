04f67ffafd077ec07596e6b242a89d335ff61f2b
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-08-22 08:15:12 +1000

    Print bytes

--- lib/src/main/kotlin/Main.kt
+++ lib/src/main/kotlin/Main.kt
@@ -7,9 +7,16 @@ fun main() {
 
     val powerpuffGirl = PowerpuffGirl.Builder()
         .id(id = 1)
-        .name(name = "")
+        .name(name = "Bubble")
         .type(type = PowerpuffGirl.Type.BUBBLE)
         .abilities(listOf(FLIGHT, SPEED))
         .build()
 
+    println(powerpuffGirl.encodeByteString())
+    println("size: ${powerpuffGirl.encodeByteString().size} bytes")
 }
+
+/**
+ * [hex=08011206427562626c65180218042000]
+ * size: 16 bytes
+ */
